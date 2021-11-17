8b0cb0eb4d009c1a4a07574aff0779d6e5ef82d4
Himanshu Soni <63811466+L00kAhead@users.noreply.github.com>
2021-11-17 14:52:47 +0500

    project setup
    - created the project skeleton
    - Used "npx" instead of npm as it is a part of npm (Node Package Manager) except npx runs a remote script instead of installing it locally. This is done so that project always have the latest version of React installed in project.
    - setup firebase
    -deleted test files and logo.svg from webstore/src/

---
+++ webstore/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAGr6ji_El4fjzH34rZH39MYyoOVFFhhBg",
+    authDomain: "ecom-website-c97b3.firebaseapp.com",
+    projectId: "ecom-website-c97b3",
+    storageBucket: "ecom-website-c97b3.appspot.com",
+    messagingSenderId: "1038540511560",
+    appId: "1:1038540511560:web:010b4c7982967f39a37b7d",
+    measurementId: "G-1231NVGKET"
+  };
